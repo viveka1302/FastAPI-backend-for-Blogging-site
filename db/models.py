08292d77a8760f6eafd15d4ad6ba30e1696a77a7
Vivek Anand <78247712+viveka1302@users.noreply.github.com>
2024-03-15 12:38:56 +0530

    Add files via upload

---
+++ db/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from db.database import Base
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
+class DbUser(Base):
+    __tablename__= "users"
+    id= Column(Integer, primary_key=True, index=True)
+    username= Column(String)
+    email= Column(String)
+    password= Column(String)
+    items= relationship("DbArticle", back_populates="user")
+
+class DbArticle(Base):
+    __tablename__= "articles"
+    id= Column(Integer, primary_key=True, index= True)
+    title= Column(String)
+    content = Column(String)
+    published = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user= relationship("DbUser", back_populates="items")
